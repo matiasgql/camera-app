37f7a74f9093aec1f7d5a6eda619dc680cc5d803
matiasgql <matiasql999@gmail.com>
2023-01-31 19:26:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function cameraStart() {
         .getUserMedia(
             {
             video: {
-              facingMode: "user",
+              facingMode: "environment",
             },
           })
         .then(function(stream) {
