be14d4fa958e12a878dda2551048d373f2a99c0b
matiasgql <matiasql999@gmail.com>
2023-01-31 18:29:23 -0300

    Now uses the rear camera

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 // Define constants
-const cameraView = document.querySelector("#camera--view");
+const cameraView = document.querySelector("#camera--view");  
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
-        .getUserMedia(constraints)
+        .getUserMedia(
+            {
+            video: {
+              facingMode: "environment",
+            },
+          })
         .then(function(stream) {
             track = stream.getTracks()[0];
             cameraView.srcObject = stream;
         });
 };
+
+cameraView.requestFullscreen()
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
