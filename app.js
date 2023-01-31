6d2f4dfd24a02a04ed48ac8dbdef364afb66b0ac
matiasgql <matiasql999@gmail.com>
2023-01-31 19:37:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ var track = null;
 // Define constants
 const cameraView = document.querySelector("#camera--view");  
 
+const constraints = 
+{ 
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440,
+    },
+    frameRate: 60,
+    facingMode: "user"
+  },
+};
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
-        .getUserMedia(
-            {
-            video: {
-              facingMode: "environment",
-            },
-          })
+        .getUserMedia(constraints)
         .then(function(stream) {
             track = stream.getTracks()[0];
             cameraView.srcObject = stream;
